529ddea707242a195852faf457d191564de0b384
ilia pugachev <puagchev.ilay@gmail.com>
2025-06-23 14:52:38 +0400

    Added ability to create service account

--- init/init.sh
+++ init/init.sh
@@ -13,13 +13,13 @@ cat > /tmp/policy.json <<EOF
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
       "Effect": "Allow",
+      "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
       "Resource": "arn:aws:s3:::${MINIO_BUCKET}"
     },
     {
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
       "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
       "Resource": "arn:aws:s3:::${MINIO_BUCKET}/*"
     }
   ]
@@ -28,21 +28,8 @@ EOF
 
 mc admin policy create local readonly-obsidian /tmp/policy.json || true
 
-echo "👤 Creating user '$MINIO_USER'…"
-mc admin user add local "$MINIO_USER" "$MINIO_PASSWORD" || true
-
-echo "🔒 Attaching policy to user…"
-mc admin policy attach local readonly-obsidian --user "$MINIO_USER" || true
-
-echo "🔍 Verifying access for new user…"
-mc alias set obsidian http://localhost:9000 "$MINIO_USER" "$MINIO_PASSWORD"
-
-# Testing read/write/delete bucket
-echo "TestFile" > /tmp/testfile.txt
-mc cp /tmp/testfile.txt obsidian/"$MINIO_BUCKET"/testfile.txt
-mc cat obsidian/"$MINIO_BUCKET"/testfile.txt >/dev/null
-mc rm obsidian/"$MINIO_BUCKET"/testfile.txt
-
-rm -f /tmp/testfile.txt
-
-echo "✅ User access verified (upload/read/delete)."
+echo "🔐 Creating service account for Obsidian"
+mc admin user svcacct add local "$MINIO_ROOT_USER" \
+  --access-key "$MINIO_ACCESS_KEY" \
+  --secret-key "$MINIO_SECRET_KEY" \
+  --policy "$POLICY_NAME"
